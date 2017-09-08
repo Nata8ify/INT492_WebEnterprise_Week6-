b8d6efad16b8eef7e01c16a99e012e09012bde14
NATTAWUT PICHAIWATTNPHON <Nata8ify@users.noreply.github.com>
2017-09-08 14:56:52 +0700

    MAKE Logger

--- src/main/java/com/arms/RestClientTemplateApplication.java
+++ src/main/java/com/arms/RestClientTemplateApplication.java
@@ -1,5 +1,7 @@
 package com.arms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +9,12 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestClientTemplateApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(RestClientTemplateApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(RestClientTemplateApplication.class, args);
 		final String URI = "http://localhost:8080/todos";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> listResponse = restTemplate.getForEntity(URI, String.class);
+		logger.info(listResponse.getBody());
 	}
 }
